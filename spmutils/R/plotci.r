e61a6465a63b98fde73309d41097f58de5ad3c60
Michael Peck <mlpeck54@gmail.com>
2026-06-22 14:58:57 -0500

    added alpha to plotci

--- spmutils/R/plotci.r
+++ spmutils/R/plotci.r
@@ -1,20 +1,22 @@
-plotci <- function(df, x, y, x.sd=NULL, xmin=NULL, xmax=NULL, y.sd=NULL, ymin=NULL, ymax=NULL, color=NULL) {
+plotci <- function(df, x, y, x.sd=NULL, xmin=NULL, xmax=NULL,
+                   y.sd=NULL, ymin=NULL, ymax=NULL,
+                   color=NULL, alpha=1) {
   require(ggplot2)
   attach(df)
   on.exit(detach(df))
 
   g1 <- ggplot(df, aes(x={{x}}, y={{y}}, color={{color}})) + geom_point(na.rm=TRUE)
   if (!is.null(x.sd)) {
-    g1 <- g1 + geom_errorbarh(aes(y={{y}}, xmin={{x}}-{{x.sd}}, xmax={{x}}+{{x.sd}}, color={{color}}))
+    g1 <- g1 + geom_errorbarh(aes(y={{y}}, xmin={{x}}-{{x.sd}}, xmax={{x}}+{{x.sd}}, color={{color}}), alpha=alpha)
   }
   if (!is.null(xmin) && !is.null(xmax)) {
-    g1 <- g1 + geom_errorbarh(aes(y={{y}}, xmin={{xmin}}, xmax={{xmax}}, color={{color}}))
+    g1 <- g1 + geom_errorbarh(aes(y={{y}}, xmin={{xmin}}, xmax={{xmax}}, color={{color}}), alpha=alpha)
   }
   if (!is.null(y.sd)) {
-    g1 <- g1 + geom_errorbar(aes(x={{x}}, ymin={{y}}-{{y.sd}}, ymax={{y}}+{{y.sd}}, color={{color}}))
+    g1 <- g1 + geom_errorbar(aes(x={{x}}, ymin={{y}}-{{y.sd}}, ymax={{y}}+{{y.sd}}, color={{color}}), alpha=alpha)
   }
   if (!is.null(ymin) && !is.null(ymax)) {
-    g1 <- g1 + geom_errorbar(data=df, mapping=aes(x={{x}}, ymin={{ymin}}, ymax={{ymax}}, color={{color}}))
+    g1 <- g1 + geom_errorbar(data=df, mapping=aes(x={{x}}, ymin={{ymin}}, ymax={{ymax}}, color={{color}}), alpha=alpha)
   }
   g1
 }
